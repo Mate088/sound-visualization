c7cadb0f801842a55fa8be13144e49a47f485834
Mate088 <matstudy60@gmail.com>
2024-02-26 01:04:31 +0300

    Try to fix CORS [WIP]

--- js/index.js
+++ js/index.js
@@ -15,41 +15,39 @@ analyser.connect(audioCtx.destination)
 
 let frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
+window.onload = function() { 
 
-document.getElementById('player-btn').addEventListener('click', function() {
-    if(!this.classList.contains('play-btn__play')) {
-        player.play();
-        this.textContent = 'Pause';
-        this.classList.add('play-btn__play');
-    } else {
-        player.pause();
-        this.textContent = 'Play';
-        this.classList.remove('play-btn__play');       
+    document.getElementById('player-btn').addEventListener('click', function() {
+        if(!this.classList.contains('play-btn__play')) {
+            player.play();
+            this.textContent = 'Pause';
+            this.classList.add('play-btn__play');
+        } else {
+            player.pause();
+            this.textContent = 'Play';
+            this.classList.remove('play-btn__play');       
+        }
+    })
+
+    window.addEventListener('resize', resizeCanvas, false);
+
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
     }
-})
 
-window.addEventListener('resize', resizeCanvas, false);
+    resizeCanvas();
 
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-
-
-
-
-resizeCanvas();
-
-function drawColumn(x, width, height) {
-    const gradient = ctx.createLinearGradient(0, canvas.height - height / 2, 0, canvas.height);
-    gradient.addColorStop(1, 'rgba(255, 255, 255, 1)');
+    function drawColumn(x, width, height) {
+        const gradient = ctx.createLinearGradient(0, canvas.height - height / 2, 0, canvas.height);
+        gradient.addColorStop(1, 'rgba(255, 255, 255, 1)');
     gradient.addColorStop(0.9, 'rgba(255, 150, 0, 1)');
-    gradient.addColorStop(0, 'rgba(255, 0, 0, 0)');
-    ctx.fillStyle = gradient;
-    ctx.fillRect(x, canvas.height - height / 2, width, height);
-}
+        gradient.addColorStop(0, 'rgba(255, 0, 0, 0)');
+        ctx.fillStyle = gradient;
+        ctx.fillRect(x, canvas.height - height / 2, width, height);
+    }
 
-function render() {
+    function render() {
 
     analyser.getByteFrequencyData(frequencyData);
 
@@ -70,6 +68,13 @@ function render() {
 
     window.requestAnimationFrame(render)
 
+    }
+
+    window.requestAnimationFrame(render);
 }
 
-window.requestAnimationFrame(render);
+document.querySelector('button').addEventListener('click', function() {
+    audioCtx.resume().then(() => {
+      console.log('Playback resumed successfully');
+    });
+  });
