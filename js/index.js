a52969fbedfc2d4d8d5fa33f3479d9cb1d9295a4
Mate088 <matstudy60@gmail.com>
2025-07-22 21:27:37 +0300

    Playing with columnsGap and columnCount

--- js/index.js
+++ js/index.js
@@ -1,80 +1,85 @@
-const columnsGap = 10;
-const columnCount = 256;
+const columnsGap = 30;
+const columnCount = 2048;
 
-const canvas = document.getElementById('player-fireplace')
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("player-fireplace");
+const ctx = canvas.getContext("2d");
 
-const player = document.getElementById('audio-player');
+const player = document.getElementById("audio-player");
 
-let audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 let source = audioCtx.createMediaElementSource(player);
 let analyser = audioCtx.createAnalyser();
 analyser.fftSize = columnCount;
 source.connect(analyser);
-analyser.connect(audioCtx.destination)
+analyser.connect(audioCtx.destination);
 
 let frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
-window.onload = function() { 
-
-    document.getElementById('player-btn').addEventListener('click', function() {
-        if(!this.classList.contains('play-btn__play')) {
-            player.play();
-            this.textContent = 'Pause';
-            this.classList.add('play-btn__play');
-        } else {
-            player.pause();
-            this.textContent = 'Play';
-            this.classList.remove('play-btn__play');       
-        }
-    })
-
-    window.addEventListener('resize', resizeCanvas, false);
-
-    function resizeCanvas() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+window.onload = function () {
+  document.getElementById("player-btn").addEventListener("click", function () {
+    if (!this.classList.contains("play-btn__play")) {
+      player.play();
+      this.textContent = "Pause";
+      this.classList.add("play-btn__play");
+    } else {
+      player.pause();
+      this.textContent = "Play";
+      this.classList.remove("play-btn__play");
     }
+  });
 
-    resizeCanvas();
+  window.addEventListener("resize", resizeCanvas, false);
 
-    function drawColumn(x, width, height) {
-        const gradient = ctx.createLinearGradient(0, canvas.height - height / 2, 0, canvas.height);
-        gradient.addColorStop(1, 'rgba(255, 255, 255, 1)');
-    gradient.addColorStop(0.9, 'rgba(255, 150, 0, 1)');
-        gradient.addColorStop(0, 'rgba(255, 0, 0, 0)');
-        ctx.fillStyle = gradient;
-        ctx.fillRect(x, canvas.height - height / 2, width, height);
-    }
+  function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
 
-    function render() {
+  resizeCanvas();
 
+  function drawColumn(x, width, height) {
+    const gradient = ctx.createLinearGradient(
+      0,
+      canvas.height - height / 2,
+      0,
+      canvas.height
+    );
+    gradient.addColorStop(1, "rgba(255, 255, 255, 1)");
+    gradient.addColorStop(0.9, "rgba(255, 150, 0, 1)");
+    gradient.addColorStop(0, "rgba(255, 0, 0, 0)");
+    ctx.fillStyle = gradient;
+    ctx.fillRect(x, canvas.height - height / 2, width, height);
+  }
+
+  function render() {
     analyser.getByteFrequencyData(frequencyData);
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    const columnWidth = (canvas.width / frequencyData.length) - columnsGap + (columnsGap / frequencyData.length);
+    const columnWidth =
+      canvas.width / frequencyData.length -
+      columnsGap +
+      columnsGap / frequencyData.length;
     const heightScale = canvas.height / 100;
 
     let xPos = 0;
 
     for (let i = 0; i < frequencyData.length; i++) {
-        let columnHeight = frequencyData[i] * heightScale;
+      let columnHeight = frequencyData[i] * heightScale;
 
-        drawColumn(xPos,columnWidth, columnHeight / 2);
-        
-        xPos += columnWidth + columnsGap;
-    }
+      drawColumn(xPos, columnWidth, columnHeight / 2);
 
-    window.requestAnimationFrame(render)
-
+      xPos += columnWidth + columnsGap;
     }
 
     window.requestAnimationFrame(render);
-}
+  }
 
-document.querySelector('button').addEventListener('click', function() {
-    audioCtx.resume().then(() => {
-      console.log('Playback resumed successfully');
-    });
+  window.requestAnimationFrame(render);
+};
+
+document.querySelector("button").addEventListener("click", function () {
+  audioCtx.resume().then(() => {
+    console.log("Playback resumed successfully");
   });
+});
